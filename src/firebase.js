c5d55d6f1af2945c0ab70a7756864f93f012360f
yoniOudmayer <127691888+yoniOudmayer@users.noreply.github.com>
2023-04-05 16:39:30 +0200

    Updating

--- src/firebase.js
+++ src/firebase.js
@@ -21,8 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
 export default app;
-export {db};
 export { auth };
