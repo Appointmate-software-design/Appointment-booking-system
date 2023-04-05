e09fd8cc3d5e034a54e78c14ca2309ca9ce08d0a
yoniOudmayer <127691888+yoniOudmayer@users.noreply.github.com>
2023-04-05 16:41:10 +0200

    Updating

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import SignUp from "./components/SignUp";
 import Title from "./components/Title";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import CreateEventForm from './components/CreateEventForm'
 import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import ConfirmBookingView from './components/ConfirmBookingView'
 import PrivateRoute from "./components/PrivateRoute"; //this is so we can't acces the home page when we log out
 function App() {
   return (
@@ -12,9 +14,11 @@ function App() {
         <Router>
       <AuthProvider>
         <Routes>
-        <Route path="/*" element={<PrivateRoute><Home/></PrivateRoute>} />
+        <Route path="/" element={<PrivateRoute><Home/></PrivateRoute>} />
+        <Route path ='/create-event' element={<PrivateRoute><CreateEventForm/></PrivateRoute>}/>
         <Route path="/signup" element={<SignUp/>}/>
         <Route path ="/login" element={<Login/>}/>
+        <Route path = 'events/:eventId' element={<ConfirmBookingView/>}/>
         </Routes>
       </AuthProvider>
       </Router>
