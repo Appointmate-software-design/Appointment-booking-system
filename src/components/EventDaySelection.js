86d12965eb3952b96168e3b2d7b08fdf5b8f9800
yoniOudmayer <127691888+yoniOudmayer@users.noreply.github.com>
2023-04-08 20:44:44 +0200

    Update EventDaySelection.js

--- src/components/EventDaySelection.js
+++ src/components/EventDaySelection.js
@@ -2,6 +2,9 @@ import React from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+//this file handles our datepicker, on the confirmation booking page
+// when someone selects a a date to book 
+
 export default function EventDaySelection({ handleSelectDay, selectedDate, startDate, endDate, minDate, maxDate }) {
     return (
         <div>
