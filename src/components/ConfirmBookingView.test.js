61fc27bfb767d63f2f32285d8cdc6a3b662b38b0
DanielTeeger <daniel.teeger1@students.wits.ac.za>
2023-04-21 00:48:39 +0200

    Added some more testing

--- src/components/ConfirmBookingView.test.js
+++ src/components/ConfirmBookingView.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+\import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import ConfirmBookingView from './ConfirmBookingView';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -12,26 +12,48 @@ jest.mock('react-router-dom', () => ({
 }));
 
 // Mock data for the useDocumentData hook
+// Mock data for the useDocumentData hook
 jest.mock('react-firebase-hooks/firestore', () => ({
-  useDocumentData: () => [
-    {
-      title: 'Test Event',
-      description: 'Test Description',
-      startDate: '2022-01-01',
-      endDate: '2022-12-31',
-      duration: 30,
-      checkedDays: [
-        {
-          day: 'Monday',
-          startTime: '09:00',
-          endTime: '17:00',
-        },
-      ],
-    },
-    false,
-    null,
-  ],
-}));
+    useDocumentData: () => [
+      {
+        title: 'Test Event',
+        description: 'Test Description',
+        startDate: '2022-01-01',
+        endDate: '2022-12-31',
+        duration: 30,
+        checkedDays: [
+          {
+            day: 'Monday',
+            startTime: '06:00',
+            endTime: '18:00',
+          },
+          {
+            day: 'Tuesday',
+            startTime: '06:00',
+            endTime: '18:00',
+          },
+          {
+            day: 'Wednesday',
+            startTime: '06:00',
+            endTime: '18:00',
+          },
+          {
+            day: 'Thursday',
+            startTime: '06:00',
+            endTime: '18:00',
+          },
+          {
+            day: 'Friday',
+            startTime: '06:00',
+            endTime: '18:00',
+          },
+        ],
+      },
+      false,
+      null,
+    ],
+  }));
+  
 
 describe('ConfirmBookingView', () => {
   it('renders without crashing', () => {
@@ -43,5 +65,46 @@ describe('ConfirmBookingView', () => {
     expect(getByText('Meeting Details:')).toBeInTheDocument();
   });
 
-  // Add more test cases here
+  it('renders the correct information', () => {
+    const { getByText } = render(
+      <Router>
+        <ConfirmBookingView />
+      </Router>
+    );
+    expect(getByText('Title: Test Event')).toBeInTheDocument();
+    expect(getByText('Description: Test Description')).toBeInTheDocument();
+    expect(getByText('The duration of each slot is 30 minutes')).toBeInTheDocument();
+  });
+
+  it('displays an error message when selecting an unavailable day', async () => {
+    const { getByText, getByLabelText } = render(
+      <Router>
+        <ConfirmBookingView />
+      </Router>
+    );
+
+    fireEvent.click(getByLabelText('next month'));
+    fireEvent.click(getByText('1')); // Select a day that is not in the available days (e.g., Sunday)
+
+    await waitFor(() => {
+      expect(getByText('The day selected is not in the meeting schedule')).toBeInTheDocument();
+    });
+  });
+
+  it('displays available time slots when selecting an available day', async () => {
+    const { getByText, getByLabelText } = render(
+      <Router>
+        <ConfirmBookingView />
+      </Router>
+    );
+
+    fireEvent.click(getByLabelText('next month'));
+    fireEvent.click(getByText('2')); // Select a day that is in the available days (e.g., Monday)
+
+    await waitFor(() => {
+      expect(getByText('09:00-09:30')).toBeInTheDocument();
+      expect(getByText('09:30-10:00')).toBeInTheDocument();
+      // ... Add more time slots here, if necessary
+    });
+  });
 });
