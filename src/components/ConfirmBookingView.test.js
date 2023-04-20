e42cbcbfd13ec05358043343eea30f549c846582
DanielTeeger <daniel.teeger1@students.wits.ac.za>
2023-04-21 01:54:53 +0200

    Tried to get all test cases working for ConfirmBookingView

--- src/components/ConfirmBookingView.test.js
+++ src/components/ConfirmBookingView.test.js
@@ -1,111 +1,127 @@
-import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import ConfirmBookingView from './ConfirmBookingView';
-import { BrowserRouter as Router } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import ConfirmBookingView from "./ConfirmBookingView";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import EventDaySelection from "./EventDaySelection";
+import { auth } from "../firebase";
 
-// Mock data for the useParams hook
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: () => ({
-    eventId: '123',
-  }),
+jest.mock("react-firebase-hooks/firestore", () => ({
+  useDocumentData: jest.fn(),
 }));
 
-// Mock data for the useDocumentData hook
-// Mock data for the useDocumentData hook
-jest.mock('react-firebase-hooks/firestore', () => ({
-    useDocumentData: () => [
+jest.mock("./EventDaySelection", () => {
+  return {
+    __esModule: true,
+    default: jest.fn(() => <div data-testid="event-day-selection-mock" />),
+  };
+});
+
+jest.mock("../firebase", () => ({
+  auth: {
+    currentUser: {
+      uid: "test-uid",
+    },
+  },
+}));
+
+describe("ConfirmBookingView", () => {
+  it("renders loading state", () => {
+    useDocumentData.mockReturnValue([null, true, null]);
+
+    render(<ConfirmBookingView />);
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("renders error state", async () => {
+    useDocumentData.mockReturnValue([null, false, new Error("Test error")]);
+
+    render(<ConfirmBookingView />);
+
+    await waitFor(() => screen.getByText("there was an error getting event"));
+  });
+
+  it("handles successful booking", async () => {
+    useDocumentData.mockReturnValue([
       {
-        title: 'Test Event',
-        description: 'Test Description',
-        startDate: '2022-01-01',
-        endDate: '2022-12-31',
-        duration: 30,
+        title: "Test Event",
+        description: "Test Description",
+        duration: 60,
+        startDate: "2023-04-20",
+        endDate: "2023-04-30",
         checkedDays: [
           {
-            day: 'Monday',
-            startTime: '06:00',
-            endTime: '18:00',
-          },
-          {
-            day: 'Tuesday',
-            startTime: '06:00',
-            endTime: '18:00',
-          },
-          {
-            day: 'Wednesday',
-            startTime: '06:00',
-            endTime: '18:00',
-          },
-          {
-            day: 'Thursday',
-            startTime: '06:00',
-            endTime: '18:00',
-          },
-          {
-            day: 'Friday',
-            startTime: '06:00',
-            endTime: '18:00',
+            day: "Monday",
+            startTime: "10:00",
+            endTime: "18:00",
           },
         ],
       },
       false,
       null,
-    ],
-  }));
-  
+    ]);
 
-describe('ConfirmBookingView', () => {
-  it('renders without crashing', () => {
-    const { getByText } = render(
-      <Router>
-        <ConfirmBookingView />
-      </Router>
-    );
-    expect(getByText('Meeting Details:')).toBeInTheDocument();
+    render(<ConfirmBookingView />);
+
+    await waitFor(() => screen.getByText("Test Event"));
+
+    // Simulate user interactions
+    fireEvent.change(screen.getByLabelText("Email:"), { target: { value: "test@example.com" } });
+    fireEvent.change(screen.getByLabelText("Name:"), { target: { value: "John" } });
+
+    fireEvent.click(screen.getByText("no date selected"));
+
+    const eventDaySelectionMock = screen.getByTestId("event-day-selection-mock");
+    fireEvent.change(eventDaySelectionMock, { target: { value: new Date("2023-04-24") } });
+
+    const timeSlotCheckbox = screen.getByLabelText("10:00-11:00");
+    fireEvent.click(timeSlotCheckbox);
+
+    const confirmBookingButton = screen.getByText("Confirm booking");
+    fireEvent.click(confirmBookingButton);
+
+    await waitFor(() => screen.getByText("Booking successful!"));
   });
 
-  it('renders the correct information', () => {
-    const { getByText } = render(
-      <Router>
-        <ConfirmBookingView />
-      </Router>
-    );
-    expect(getByText('Title: Test Event')).toBeInTheDocument();
-    expect(getByText('Description: Test Description')).toBeInTheDocument();
-    expect(getByText('The duration of each slot is 30 minutes')).toBeInTheDocument();
-  });
+  it("handles unsuccessful booking due to invalid email", async () => {
+    useDocumentData.mockReturnValue([
+      {
+        title: "Test Event",
+        description: "Test Description",
+        duration: 60,
+        startDate: "2023-04-20",
+        endDate: "2023-04-30",
+        checkedDays: [
+          {
+            day: "Monday",
+            startTime: "10:00",
+            endTime: "18:00",
+          },
+        ],
+      },
+      false,
+      null,
+    ]);
 
-  it('displays an error message when selecting an unavailable day', async () => {
-    const { getByText, getByLabelText } = render(
-      <Router>
-        <ConfirmBookingView />
-      </Router>
-    );
+    render(<ConfirmBookingView />);
 
-    fireEvent.click(getByLabelText('next month'));
-    fireEvent.click(getByText('1')); // Select a day that is not in the available days (e.g., Sunday)
+    await waitFor(() => screen.getByText("Test Event"));
 
-    await waitFor(() => {
-      expect(getByText('The day selected is not in the meeting schedule')).toBeInTheDocument();
-    });
-  });
+    // Simulate user interactions
+    fireEvent.change(screen.getByLabelText("Email:"), { target: { value: "invalid-email" } });
+    fireEvent.change(screen.getByLabelText("Name:"), { target: { value: "John" } });
 
-  it('displays available time slots when selecting an available day', async () => {
-    const { getByText, getByLabelText } = render(
-      <Router>
-        <ConfirmBookingView />
-      </Router>
-    );
+    fireEvent.click(screen.getByText("no date selected"));
 
-    fireEvent.click(getByLabelText('next month'));
-    fireEvent.click(getByText('2')); // Select a day that is in the available days (e.g., Monday)
+    const eventDaySelectionMock = screen.getByTestId("event-day-selection-mock");
+    fireEvent.change(eventDaySelectionMock, { target: { value: new Date("2023-04-24") } });
 
-    await waitFor(() => {
-      expect(getByText('09:00-09:30')).toBeInTheDocument();
-      expect(getByText('09:30-10:00')).toBeInTheDocument();
-      // ... Add more time slots here, if necessary
-    });
+    const timeSlotCheckbox = screen.getByLabelText("10:00-11:00");
+    fireEvent.click(timeSlotCheckbox);
+
+    const confirmBookingButton = screen.getByText("Confirm booking");
+    fireEvent.click(confirmBookingButton);
+
+    await waitFor(() => screen.getByText("Invalid email address."));
   });
 });
+
